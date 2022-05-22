640acff675d0734a628edc0b1bf6e7093f2d7a01
sunyata <59007465+nikhilshetty13@users.noreply.github.com>
2022-05-22 16:23:32 +0530

    Add project files.

---
+++ Razortraining/Data/ApplicationDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razortraining.Model;
+
+namespace Razortraining.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>? categories { get; set; }
+    }
+}
